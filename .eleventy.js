894100c07534b0e9dedf1fe98ca0cdbf71ffa03d
Tom Caraher <65122189+tcaraher@users.noreply.github.com>
2024-05-12 04:50:07 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
